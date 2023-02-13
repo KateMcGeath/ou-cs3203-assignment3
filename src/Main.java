19384ea1b37718a2daca4041327b560cac266bc6
KateMcGeath <katemcgeath@Kates-MacBook-Pro-2.local>
2023-02-13 14:26:21 -0600

    Made a function that returns the product of an array.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,13 @@ public class Main {
 
         int array[] = {1, 2, 3, 4};
         int sum = 0;
+        int product = 0;
 
         sum = sum(array);
         System.out.println("The sum of the array is: " + sum);
+
+        product = multiply(array);
+        System.out.println("The product of the inputted array is: " + product);
 
     }
     //Method that takes in an array and sums up its values.
@@ -16,6 +20,19 @@ public class Main {
         for(int i = 0; i < array.length; i++) {
 
             value += array[i];
+        }
+
+        return value;
+    }
+
+    //Method that takes in an array and computes the product of the values.
+    public static int multiply(int[] array) {
+
+        int value = 1;
+
+        for(int i = 0; i < array.length; i++) {
+
+            value *= array[i];
         }
 
         return value;
